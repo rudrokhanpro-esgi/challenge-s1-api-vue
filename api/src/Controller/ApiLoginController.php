af07a2de5a126505d48c0b2a715b4d2a268c3bb7
Rudro Khan <rudrokhan.pro@gmail.com>
2023-07-26 04:43:32 -0700

    setup+feature: cleanup and add of login endpoint

---
+++ api/src/Controller/ApiLoginController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+use App\Entity\User;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+
+class ApiLoginController extends AbstractController
+{
+}
